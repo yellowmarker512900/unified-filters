745ff2656c1ed89ec521f7ff8243e9540c1da8be
yellowmarker2144 <167545935+yellowmarker512900@users.noreply.github.com>
2025-04-25 15:30:37 +0200

    Create remove_duplicates.py

---
+++ filters/remove_duplicates.py
@@ -0,0 +1,10 @@
+# remove_duplicates.py
+with open('filters/unified.txt', 'r') as f:
+    filters = f.readlines()
+
+# Entferne doppelte Filter
+filters_set = set(filters)
+
+# Schreibe die bereinigte Liste zurück in die Datei
+with open('filters/unified.txt', 'w') as f:
+    f.writelines(filters_set)
