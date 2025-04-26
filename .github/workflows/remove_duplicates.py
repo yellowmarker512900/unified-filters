cc299f776be1509a8ec13a07f21d7f0d5f547ca4
yellowmarker2144 <167545935+yellowmarker512900@users.noreply.github.com>
2025-04-26 18:08:50 +0200

    Update remove_duplicates.py

--- .github/workflows/remove_duplicates.py
+++ .github/workflows/remove_duplicates.py
@@ -1,12 +1,12 @@
 # Beispiel Python-Skript, das doppelte Filter aus einer Liste entfernt
 
 # remove_duplicates.py
-with open('filters/unified.txt', 'r') as f:
+with open('filters/essentials.txt', 'r') as f:
     filters = f.readlines()
 
 # Entferne doppelte Filter
 filters_set = set(filters)
 
 # Schreibe die bereinigte Liste zurück in die Datei
-with open('filters/unified.txt', 'w') as f:
+with open('filters/essentials', 'w') as f:
     f.writelines(filters_set)
